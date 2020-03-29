35d346f8d19e722c4dcdcc7253e10c59dda2a2b5
Dariusz Górak <darekg11@tlen.pl>
2020-03-29 18:05:49 +0200

    Add support for LDH A, (n)

---
+++ core/src/components/cpu/opcodes/LOAD/LOAD_TO_REG_A_VALUE_OF_MEMORY_INDEX_FROM_NEXT_MEMORY_CELL/index.spec.ts
@@ -0,0 +1,45 @@
+import handle from "./index";
+import CPU from "../../../cpu";
+import Memory from "../../../../memory/memory";
+
+describe("LOAD_TO_REG_A_VALUE_OF_MEMORY_INDEX_FROM_NEXT_MEMORY_CELL", () => {
+    test("Should set value correctly and increase PC by 2", () => {
+        const TEST_VALUE = 198;
+        const NEXT_MEMORY_CELL_VALUE = 0xA0;
+        const TEST_INDEX = 0xFFA0;
+        const cpu = new CPU();
+        const memory = new Memory();
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        memory.write8BitsValue(1, NEXT_MEMORY_CELL_VALUE);
+        memory.write8BitsValue(TEST_INDEX, TEST_VALUE)
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(TEST_VALUE);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(TEST_VALUE << 8);
+        expect(cpu.getProgramCounter()).toBe(2);
+    });
+})
---
+++ core/src/components/cpu/opcodes/LOAD/LOAD_TO_REG_A_VALUE_OF_MEMORY_INDEX_FROM_NEXT_MEMORY_CELL/index.ts
@@ -0,0 +1,25 @@
+import { IOpCodeHanlePayload } from "../../types";
+import { numberUtils } from "../../../../../utils";
+
+/*
+  OP Code: 0xF0
+  Memonic: LDH A, (n)
+  Description: Loads to register A value (2 byte === 16 bits) from next memory cell (PC++)
+  The full 16-bit absolute address is obtained by setting the most significant byte to
+  0xFF and the least significant byte to the value of n, so the possible range is 0xFF00-0xFFFF.
+  Size: 2 Byte - increments PC by 2
+  Cycles: 3
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const currentProgramCounter = payload.CPU.getProgramCounter();
+    const memoryIndexToRead = currentProgramCounter + 1;
+    const firstHalfOfMemoryIndex = 0xFF;
+    const secondHalfOfMemoryIndex = payload.Memory.read8BitsValue(memoryIndexToRead);
+    const index = numberUtils.combineTwo8BitsNumbersInto16BitsNumber(firstHalfOfMemoryIndex, secondHalfOfMemoryIndex);
+    const value = payload.Memory.read8BitsValue(index);
+    payload.CPU.setRegisterAValue(value);
+    payload.CPU.increaseProgramCounter(2);
+}
+
+export default handle;
