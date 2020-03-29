d48efe06a0c72d717b30e924e97a6d6f7591bf59
Dariusz Górak <darekg11@tlen.pl>
2020-03-29 17:35:26 +0200

    Add support for LD A, (C)

---
+++ core/src/components/cpu/opcodes/LOAD/LOAD_TO_REG_A_VALUE_OF_MEMORY_CELL_FROM_C_REGISTER/index.ts
@@ -0,0 +1,23 @@
+import { IOpCodeHanlePayload } from "../../types";
+import { numberUtils } from "../../../../../utils";
+
+/*
+  OP Code: 0xF2
+  Memonic: LD A, (C)
+  Description: Loads to register A a value from memory under cell index hold in register C
+  The full 16-bit absolute address is obtained by setting the most significant byte to 0xFF and
+  the least significant byte to the value of C, so the possible range is 0xFF00-0xFFFF
+  Size: 1 Byte - increments PC by 1
+  Cycles: 8
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const firstHalfOfMemoryIndex = 0xFF;
+    const secondHalfOfMemoryIndex = payload.CPU.getRegisterCValue();
+    const index = numberUtils.combineTwo8BitsNumbersInto16BitsNumber(firstHalfOfMemoryIndex, secondHalfOfMemoryIndex);
+    const value = payload.Memory.read8BitsValue(index);
+    payload.CPU.setRegisterAValue(value);
+    payload.CPU.increaseProgramCounter(1);
+}
+
+export default handle;
