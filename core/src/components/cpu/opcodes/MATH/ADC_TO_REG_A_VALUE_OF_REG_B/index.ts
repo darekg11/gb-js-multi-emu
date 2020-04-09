1dda0f3d150662559e39ef0ffdf2064c50bb6fc9
Dariusz Górak <darekg11@tlen.pl>
2020-04-09 19:43:18 +0200

    Add ADC A, B OP Code support

---
+++ core/src/components/cpu/opcodes/MATH/ADC_TO_REG_A_VALUE_OF_REG_B/index.ts
@@ -0,0 +1,36 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0x88
+  Memonic: ADC A, B
+  Description: Adds to register A a value of register B. Adds carry flag value to register A.
+    Sets SUBTRACTION flag to 0
+    Sets CARRY flag if we overflow 255 value
+    Sets ZERO flag if result is 0
+    Sets HALF_CARRY flag if bit 3 overflows into bit 4 so whenever there is overflow to upper nibble
+  Size: 1 Byte - increments PC by 1
+  Cycles: 4
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerAValue = payload.CPU.getRegisterAValue();
+    const registerBValue = payload.CPU.getRegisterBValue();
+    const isCarryFlagSet = payload.CPU.isCarryFlagSet();
+    const sum = registerAValue + registerBValue + (isCarryFlagSet ? 1 : 0);
+    if (sum > 255) {
+        payload.CPU.setCarryFlag();
+    }
+    const wrappedValue = sum & 255;
+    if (wrappedValue === 0) {
+        payload.CPU.setZeroFlag();
+    }
+    const shouldSetHalfCarryFlag = (registerAValue & 0xF) + (registerBValue & 0xF) > 0xF;
+    if (shouldSetHalfCarryFlag) {
+        payload.CPU.setHalfCarryFlag();
+    }
+    payload.CPU.unsetSubtractionFlag();
+    payload.CPU.setRegisterAValue(wrappedValue);
+    payload.CPU.increaseProgramCounter();
+}
+
+export default handle;
