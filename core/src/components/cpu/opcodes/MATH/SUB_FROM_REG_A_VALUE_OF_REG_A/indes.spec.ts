0b7ca48fa0d42d82dac4ebb3661dde07a715cd42
Dariusz Górak <darekg11@tlen.pl>
2020-04-11 16:33:01 +0200

    Add SUB A, A OP Code support

---
+++ core/src/components/cpu/opcodes/MATH/SUB_FROM_REG_A_VALUE_OF_REG_A/indes.spec.ts
@@ -0,0 +1,48 @@
+import handle from "./index";
+import CPU from "../../../cpu";
+import Memory from "../../../../memory/memory";
+
+describe("SUB_FROM_REG_A_VALUE_OF_REG_A", () => {
+    test("Should subtract value of reg A from reg A, set half carry and carry flag, increase PC by 1 and Subtraction flag to 1", () => {
+        const cpu = new CPU();
+        const memory = new Memory();
+        const REG_A = 255
+        const RESULT = REG_A - REG_A;
+        const EXPECTED_F_REG_VALUE = 0b11100000
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setRegisterAValue(REG_A);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(RESULT);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(RESULT << 8 | EXPECTED_F_REG_VALUE);
+        expect(cpu.getProgramCounter()).toBe(1);
+        expect(cpu.isZeroFlagSet()).toBe(true);
+        expect(cpu.isSubtractionFlagSet()).toBe(true);
+        expect(cpu.isCarryFlagSet()).toBe(false);
+        expect(cpu.isHalfCarryFlagSet()).toBe(true);
+    });
+})
---
+++ core/src/components/cpu/opcodes/MATH/SUB_FROM_REG_A_VALUE_OF_REG_A/index.ts
@@ -0,0 +1,34 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0x97
+  Memonic: SUB A, A
+  Description: Subtracts from register A a value of register A
+    Sets SUBTRACTION flag to 1
+    Sets CARRY flag if we underflow 0 value
+    Sets ZERO flag if result is 0
+    Sets HALF_CARRY flag if bit 3 overflows into bit 4 so whenever there is overflow to upper nibble
+  Size: 1 Byte - increments PC by 1
+  Cycles: 4
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerAValue = payload.CPU.getRegisterAValue();
+    const diff = registerAValue - registerAValue;
+    if (diff < 0) {
+        payload.CPU.setCarryFlag();
+    }
+    const wrappedValue = diff & 255;
+    if (wrappedValue === 0) {
+        payload.CPU.setZeroFlag();
+    }
+    const shouldSetHalfCarryFlag = (registerAValue & 0xF) + (registerAValue & 0xF) > 0xF;
+    if (shouldSetHalfCarryFlag) {
+        payload.CPU.setHalfCarryFlag();
+    }
+    payload.CPU.setSubtractionFlag();
+    payload.CPU.setRegisterAValue(wrappedValue);
+    payload.CPU.increaseProgramCounter();
+}
+
+export default handle;
