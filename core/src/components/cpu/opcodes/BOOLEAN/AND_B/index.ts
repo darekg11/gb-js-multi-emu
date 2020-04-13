ed6db7990fa1e0a9b15916aa55dc78dd8eb28ed0
Dariusz Górak <darekg11@tlen.pl>
2020-04-13 22:39:04 +0200

    Add AND B OP Code support

---
+++ core/src/components/cpu/opcodes/BOOLEAN/AND_B/index.ts
@@ -0,0 +1,30 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0xA0
+  Memonic: AND B
+  Description: Bitwise AND of A register value and B register value - store result in register A.
+    Sets SUBTRACTION flag to 0
+    Sets CARRY flag to 0
+    Sets ZERO flag if result is 0
+    Sets HALF_CARRY flag to 1
+  Size: 1 Byte - increments PC by 1
+  Cycles: 4
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerAValue = payload.CPU.getRegisterAValue();
+    const registerBValue = payload.CPU.getRegisterBValue();
+    const value = registerAValue & registerBValue;
+    const safeValue = value & 255;
+    if (safeValue === 0) {
+        payload.CPU.setZeroFlag();
+    }
+    payload.CPU.unsetSubtractionFlag();
+    payload.CPU.unsetCarryFlag();
+    payload.CPU.setHalfCarryFlag();
+    payload.CPU.setRegisterAValue(safeValue);
+    payload.CPU.increaseProgramCounter();
+}
+
+export default handle;
