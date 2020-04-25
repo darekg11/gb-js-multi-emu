c4d7a0fccf7421ac37f8e838e520e8aa6c0664b9
Dariusz Górak <darekg11@tlen.pl>
2020-04-25 21:20:47 +0200

    Add DEC HL OP Code support

---
+++ core/src/components/cpu/opcodes/MATH/DEC_HL/index.spec.ts
@@ -0,0 +1,56 @@
+import handle from "./index";
+import CPU from "../../../cpu";
+import Memory from "../../../../memory/memory";
+
+describe("DEC HL", () => {
+    test("Should decrease HL register value and do not change flag state", () => {
+        const cpu = new CPU();
+        const memory = new Memory();
+        const REG_H = 10;
+        const REG_L = 20;
+        const EXPECTED_HL_VALUE =  REG_H << 8 | REG_L;
+        const EXPECTED_F_REG_VALUE = 0b11110000
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setRegisterHValue(REG_H);
+        cpu.setRegisterLValue(REG_L);
+        cpu.setSubtractionFlag();
+        cpu.setCarryFlag();
+        cpu.setHalfCarryFlag();
+        cpu.setZeroFlag();
+
+        expect(cpu.getRegisterHLValue()).toBe(EXPECTED_HL_VALUE);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getRegisterHValue()).toBe(REG_H);
+        expect(cpu.getRegisterLValue()).toBe(REG_L - 1);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(EXPECTED_HL_VALUE - 1);
+        expect(cpu.getRegisterAFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getProgramCounter()).toBe(1);
+        expect(cpu.isZeroFlagSet()).toBe(true);
+        expect(cpu.isSubtractionFlagSet()).toBe(true);
+        expect(cpu.isCarryFlagSet()).toBe(true);
+        expect(cpu.isHalfCarryFlagSet()).toBe(true);
+    });
+})
---
+++ core/src/components/cpu/opcodes/MATH/DEC_HL/index.ts
@@ -0,0 +1,24 @@
+import { IOpCodeHanlePayload } from "../../types";
+import { numberUtils } from "../../../../../utils";
+
+/*
+  OP Code: 0x2B
+  Memonic: DEC HL
+  Description: Decreases value stored in HL register. Ultimately this decreases L register value by 1.
+    Flags are not affected.
+  Size: 1 Byte - increments PC by 1
+  Cycles: 8
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerHLValue = payload.CPU.getRegisterHLValue();
+    const incremented = registerHLValue - 1;
+    const splitted = numberUtils.split16BitsNumberIntoTwo8BitsNumbers(incremented);
+    const safeBValue = splitted[0] & 255;
+    const safeCValue = splitted[1] & 255;
+    payload.CPU.setRegisterHValue(safeBValue);
+    payload.CPU.setRegisterLValue(safeCValue);
+    payload.CPU.increaseProgramCounter();
+}
+
+export default handle;
