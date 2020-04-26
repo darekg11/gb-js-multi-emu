8e3d2b5d3da9522460222926bd1d78d0ab50db29
Dariusz Górak <darekg11@tlen.pl>
2020-04-26 19:15:12 +0200

    Add ADD HL BC OP Code support

---
+++ core/src/components/cpu/opcodes/MATH/ADD_HL_BC/index.spec.ts
@@ -0,0 +1,93 @@
+import handle from "./index";
+import CPU from "../../../cpu";
+import Memory from "../../../../memory/memory";
+
+describe("ADD_HL_BC", () => {
+    test("Should add value of reg BC to reg HL, increase PC by 1 and Subtraction flag to 0", () => {
+        const cpu = new CPU();
+        const memory = new Memory();
+        const REG_HL = 2;
+        const REG_BC = 10;
+        const RESULT = REG_HL + REG_BC;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setRegisterHLValue(REG_HL);
+        cpu.setRegisterBCValue(REG_BC);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(REG_BC >> 8);
+        expect(cpu.getRegisterCValue()).toBe(REG_BC & 0x00FF);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(RESULT >> 8);
+        expect(cpu.getRegisterLValue()).toBe(RESULT & 0x00FF);
+        expect(cpu.getRegisterBCValue()).toBe(REG_BC);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(RESULT);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(1);
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(false);
+        expect(cpu.isHalfCarryFlagSet()).toBe(false);
+    });
+
+    test("Result of 65536 should set carry flag, increase PC by 1 and Subtraction flag to 0", () => {
+        const cpu = new CPU();
+        const memory = new Memory();
+        const REG_HL = 65535;
+        const REG_BC = 1;
+        const EXPECTED_F_REG_VALUE = 0b00110000;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setRegisterHLValue(REG_HL);
+        cpu.setRegisterBCValue(REG_BC);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(REG_BC >> 8);
+        expect(cpu.getRegisterCValue()).toBe(REG_BC & 0x00FF);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(REG_BC);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getProgramCounter()).toBe(1);
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(true);
+        expect(cpu.isHalfCarryFlagSet()).toBe(true);
+    });
+})
---
+++ core/src/components/cpu/opcodes/MATH/ADD_HL_BC/index.ts
@@ -0,0 +1,32 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0x09
+  Memonic: ADD HL BC
+  Description: Adds to register HL a value of register BC.
+    Sets SUBTRACTION flag to 0
+    Sets CARRY flag if we overflow 65535 value
+    Do not change ZERO flag state
+    Sets HALF_CARRY flag if bit 3 overflows into bit 4 so whenever there is overflow to upper nibble
+  Size: 1 Byte - increments PC by 1
+  Cycles: 12
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerHLValue = payload.CPU.getRegisterHLValue();
+    const registerBCValue = payload.CPU.getRegisterBCValue();
+    const sum = registerHLValue + registerBCValue;
+    if (sum > 65535) {
+        payload.CPU.setCarryFlag();
+    }
+    const wrappedValue = sum & 655535;
+    const shouldSetHalfCarryFlag = (registerHLValue & 0xF) + (registerBCValue & 0xF) > 0xF;
+    if (shouldSetHalfCarryFlag) {
+        payload.CPU.setHalfCarryFlag();
+    }
+    payload.CPU.unsetSubtractionFlag();
+    payload.CPU.setRegisterHLValue(wrappedValue);
+    payload.CPU.increaseProgramCounter();
+}
+
+export default handle;
