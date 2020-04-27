ce1d9f39bb5ade4cb6145b8f2dd15cc495764b4a
Dariusz Górak <darekg11@tlen.pl>
2020-04-27 20:09:46 +0200

    Add SUB A, (n)  OP Code support

---
+++ core/src/components/cpu/opcodes/MATH/SUB_FROM_REG_A_VALUE_FROM_NEXT_MEMORY_CELL/index.ts
@@ -0,0 +1,36 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0xD6
+  Memonic: SUB A, (n)
+  Description: Subtracts from register A a value of next memory cell (PC++).
+    Sets SUBTRACTION flag to 1
+    Sets CARRY flag if we underflow 0 value
+    Sets ZERO flag if result is 0
+    Sets HALF_CARRY flag if bit 3 overflows into bit 4 so whenever there is overflow to upper nibble
+  Size: 2 Byte - increments PC by 2
+  Cycles: 8
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerAValue = payload.CPU.getRegisterAValue();
+    const programCounter = payload.CPU.getProgramCounter();
+    const nextMemoryValue = payload.Memory.read8BitsValue(programCounter + 1);
+    const diff = registerAValue - nextMemoryValue;
+    if (diff < 0) {
+        payload.CPU.setCarryFlag();
+    }
+    const wrappedValue = diff & 255;
+    if (wrappedValue === 0) {
+        payload.CPU.setZeroFlag();
+    }
+    const shouldSetHalfCarryFlag = (registerAValue & 0xF) + (nextMemoryValue & 0xF) > 0xF;
+    if (shouldSetHalfCarryFlag) {
+        payload.CPU.setHalfCarryFlag();
+    }
+    payload.CPU.setSubtractionFlag();
+    payload.CPU.setRegisterAValue(wrappedValue);
+    payload.CPU.increaseProgramCounter(2);
+}
+
+export default handle;
