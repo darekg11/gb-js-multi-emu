35d4889f0e2a3823007a857d7fdb785f43582f19
Dariusz Górak <darekg11@tlen.pl>
2020-04-29 17:29:42 +0200

    Add ADD SP, n OP Code support

---
+++ core/src/components/cpu/opcodes/MATH/ADD_TO_SP_A_VALUE_FROM_NEXT_MEMORY_CELL/index.spec.ts
@@ -0,0 +1,96 @@
+import handle from "./index";
+import CPU from "../../../cpu";
+import Memory from "../../../../memory/memory";
+
+describe("ADD_TO_SP_A_VALUE_FROM_NEXT_MEMORY_CELL", () => {
+    test("Should add value of next memory cell to reg SP, increase PC by 2 and Subtraction flag to 0", () => {
+        const cpu = new CPU();
+        const memory = new Memory();
+        const REG_SP = 2;
+        const VALUE = 2;
+        const RESULT = REG_SP + VALUE;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setRegisterSPValue(REG_SP);
+        memory.write8BitsValue(1, VALUE);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getRegisterSPValue()).toBe(RESULT);
+        expect(cpu.getProgramCounter()).toBe(2);
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(false);
+        expect(cpu.isHalfCarryFlagSet()).toBe(false);
+    });
+
+    test("Result of 256 should set carry flag, increase PC by 2 and Subtraction flag to 0", () => {
+        const cpu = new CPU();
+        const memory = new Memory();
+        const REG_SP = 255;
+        const VALUE = 1;
+        const RESULT = 256;
+        const EXPECTED_F_REG_VALUE = 0b00110000;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setRegisterSPValue(REG_SP);
+        memory.write8BitsValue(1, VALUE);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getRegisterSPValue()).toBe(RESULT);
+        expect(cpu.getProgramCounter()).toBe(2);
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(true);
+        expect(cpu.isHalfCarryFlagSet()).toBe(true);
+    });
+})
---
+++ core/src/components/cpu/opcodes/MATH/ADD_TO_SP_A_VALUE_FROM_NEXT_MEMORY_CELL/index.ts
@@ -0,0 +1,34 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0xE8
+  Memonic: ADD SP, n
+  Description: Adds to register A a value from next memory cell (PC++).
+    Sets SUBTRACTION flag to 0
+    Sets CARRY flag if we overflow 255 value
+    Sets ZERO flag to 0
+    Sets HALF_CARRY flag if bit 3 overflows into bit 4 so whenever there is overflow to upper nibble
+  Size: 2 Byte - increments PC by 2
+  Cycles: 16
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerSPValue = payload.CPU.getRegisterSPValue();
+    const programCounter = payload.CPU.getProgramCounter();
+    const nextMemoryValue = payload.Memory.read8BitsValue(programCounter + 1);
+    const sum = registerSPValue + nextMemoryValue;
+    const wrappedValue = sum & 0xFFFF;
+    if (sum > 255) {
+        payload.CPU.setCarryFlag();
+    }
+    const shouldSetHalfCarryFlag = (registerSPValue & 0xF) + (nextMemoryValue & 0xF) > 0xF;
+    if (shouldSetHalfCarryFlag) {
+        payload.CPU.setHalfCarryFlag();
+    }
+    payload.CPU.unsetZeroFlag();
+    payload.CPU.unsetSubtractionFlag();
+    payload.CPU.setRegisterSPValue(wrappedValue);
+    payload.CPU.increaseProgramCounter(2);
+}
+
+export default handle;
