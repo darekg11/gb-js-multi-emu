6944e88bbb1e3076a4010122453fcb8fb7b54808
Dariusz Górak <darekg11@tlen.pl>
2020-04-29 17:50:58 +0200

    Add LD HL, SP + n OP Code support

---
+++ core/src/components/cpu/opcodes/LOAD/LOAD_TO_REG_HL_A_VALUE_OF_SP_REG_PLUS_VALUE_FROM_NEXT_MEMORY_CELL/index.spec.ts
@@ -0,0 +1,51 @@
+import handle from "./index";
+import CPU from "../../../cpu";
+import Memory from "../../../../memory/memory";
+
+describe("LOAD_TO_REG_HL_A_VALUE_OF_SP_REG_PLUS_VALUE_FROM_NEXT_MEMORY_CELL", () => {
+    test("Should set value correctly and increase PC by 2", () => {
+        const REG_HL = 20;
+        const REG_SP = 10;
+        const VALUE = 3;
+        const EXPECTED_VALUE = REG_SP + VALUE;
+        const cpu = new CPU();
+        const memory = new Memory();
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setRegisterHLValue(REG_HL);
+        cpu.setRegisterSPValue(REG_SP);
+        memory.write8BitsValue(1, VALUE);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(EXPECTED_VALUE >> 8);
+        expect(cpu.getRegisterLValue()).toBe(EXPECTED_VALUE & 0x00FF);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(EXPECTED_VALUE);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(2);
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isHalfCarryFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(false);
+    });
+})
---
+++ core/src/components/cpu/opcodes/LOAD/LOAD_TO_REG_HL_A_VALUE_OF_SP_REG_PLUS_VALUE_FROM_NEXT_MEMORY_CELL/index.ts
@@ -0,0 +1,35 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0xF8
+  Memonic: LD HL, SP+n
+  Description: Loads to register HL (16bits), value combined from SP register and next memory cell (PC++)
+  Flags:
+    Sets SUBTRACTION flag to 0
+    Sets CARRY flag if we overflow 255 value
+    Sets ZERO flag to 0
+    Sets HALF_CARRY flag if bit 3 overflows into bit 4 so whenever there is overflow to upper nibble
+  Size: 2 Byte - increments PC by 2
+  Cycles: 12
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const spRegisterValue = payload.CPU.getRegisterSPValue();
+    const currentProgramCounter = payload.CPU.getProgramCounter();
+    const memoryValue = payload.Memory.read8BitsValue(currentProgramCounter + 1);
+    const sum = spRegisterValue + memoryValue;
+    const wrappedValue = sum & 0xFFFF;
+    if (sum > 0xFF) {
+      payload.CPU.setCarryFlag();
+    }
+    const shouldSetHalfCarryFlag = (spRegisterValue & 0xF) + (memoryValue & 0xF) > 0xF;
+    if (shouldSetHalfCarryFlag) {
+        payload.CPU.setHalfCarryFlag();
+    }
+    payload.CPU.unsetZeroFlag();
+    payload.CPU.unsetSubtractionFlag();
+    payload.CPU.setRegisterHLValue(wrappedValue);
+    payload.CPU.increaseProgramCounter(2);
+}
+
+export default handle;
