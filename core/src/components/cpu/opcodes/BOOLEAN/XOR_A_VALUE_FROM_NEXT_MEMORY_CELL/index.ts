e8e040b8a97bfae56c08d56819463f89971ed2b2
Dariusz Górak <darekg11@tlen.pl>
2020-04-29 20:49:08 +0200

    Add XOR, n OP Code support

---
+++ core/src/components/cpu/opcodes/BOOLEAN/XOR_A_VALUE_FROM_NEXT_MEMORY_CELL/index.ts
@@ -0,0 +1,31 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0xEE
+  Memonic: XOR n
+  Description: Bitwise XOR of A register value and next memory cell (PC+) - store result in register A.
+    Sets SUBTRACTION flag to 0
+    Sets CARRY flag to 0
+    Sets ZERO flag if result is 0
+    Sets HALF_CARRY flag to 0
+  Size: 2 Byte - increments PC by 2
+  Cycles: 8
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const currentProgramCounter = payload.CPU.getProgramCounter();
+    const registerAValue = payload.CPU.getRegisterAValue();
+    const memoryValue = payload.Memory.read8BitsValue(currentProgramCounter + 1);
+    const value = registerAValue ^ memoryValue;
+    const safeValue = value & 255;
+    if (safeValue === 0) {
+        payload.CPU.setZeroFlag();
+    }
+    payload.CPU.unsetSubtractionFlag();
+    payload.CPU.unsetCarryFlag();
+    payload.CPU.unsetHalfCarryFlag();
+    payload.CPU.setRegisterAValue(safeValue);
+    payload.CPU.increaseProgramCounter(2);
+}
+
+export default handle;
