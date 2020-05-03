2af0c4a4576454f9fc8e8eb908a3d109f8197b93
Dariusz Górak <darekg11@tlen.pl>
2020-05-03 19:03:13 +0200

    Add JMP C,nn Code support

---
+++ core/src/components/cpu/opcodes/JMP/JMP_IF_CARRY_FLAG_IS_SET/index.spec.ts
@@ -0,0 +1,100 @@
+import handle from "./index";
+import CPU from "../../../cpu";
+import Memory from "../../../../memory/memory";
+
+describe("JMP_IF_CARRY_FLAG_IS_SET", () => {
+    test("Should JMP if carry flag is set", () => {
+        const cpu = new CPU();
+        const memory = new Memory();
+        const PC = 10;
+        const JMP_VALUE_FIRST_HALF = 0xAA;
+        const JMP_VALUE_SECOND_HALF = 0xBB;
+        const EXPECTED_PC = (JMP_VALUE_FIRST_HALF << 8) | JMP_VALUE_SECOND_HALF;
+        const EXPECTED_F_REG_VALUE = 0b00010000;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.jump(PC);
+        cpu.setCarryFlag();
+        memory.write8BitsValue(PC + 1, JMP_VALUE_FIRST_HALF);
+        memory.write8BitsValue(PC + 2, JMP_VALUE_SECOND_HALF);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getProgramCounter()).toBe(EXPECTED_PC);
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(true);
+        expect(cpu.isHalfCarryFlagSet()).toBe(false);
+    });
+
+    test("Should not JMP if Zero Flag is not set", () => {
+        const cpu = new CPU();
+        const memory = new Memory();
+        const PC = 10;
+        const JMP_VALUE_FIRST_HALF = 0xAA;
+        const JMP_VALUE_SECOND_HALF = 0xBB;
+        const EXPECTED_PC = PC + 3;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.jump(PC);
+        cpu.unsetCarryFlag();
+        memory.write8BitsValue(PC + 1, JMP_VALUE_FIRST_HALF);
+        memory.write8BitsValue(PC + 2, JMP_VALUE_SECOND_HALF);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(EXPECTED_PC);
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(false);
+        expect(cpu.isHalfCarryFlagSet()).toBe(false);
+    });
+})
