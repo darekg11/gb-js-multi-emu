83a295ae1921c8cab8de26deacf111afe77ad9bb
Dariusz Górak <darekg11@tlen.pl>
2020-05-03 17:55:28 +0200

    Add JMP NZ nn Code support

---
+++ core/src/components/cpu/opcodes/JMP/JMP_IF_ZERO_FLAG_IS_NOT_SET/index.ts
@@ -0,0 +1,25 @@
+import { IOpCodeHanlePayload } from "../../types";
+import { numberUtils } from "../../../../../utils";
+
+/*
+  OP Code: 0xC2
+  Memonic: JMP NZ, nn
+  Description: Jump to 16 bits address combined from two next memory cells if Zero Flag is not set.
+  Size: 3 Byte - increments PC by 3 if jump is not made
+  Cycles: 16 if jump is taken, 12 if it is not
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const isZeroFlagSet = payload.CPU.isZeroFlagSet();
+    if (!isZeroFlagSet) {
+        const currentProgramCounter = payload.CPU.getProgramCounter();
+        const firstHalf = payload.Memory.read8BitsValue(currentProgramCounter + 1);
+        const secondHalf = payload.Memory.read8BitsValue(currentProgramCounter + 2);
+        const value = numberUtils.combineTwo8BitsNumbersInto16BitsNumber(firstHalf, secondHalf);
+        payload.CPU.jump(value);
+    } else {
+        payload.CPU.increaseProgramCounter(3);
+    }
+}
+
+export default handle;
