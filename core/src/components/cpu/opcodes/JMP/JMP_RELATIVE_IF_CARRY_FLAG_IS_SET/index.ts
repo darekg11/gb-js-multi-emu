d89c7550a95f02ecfe792eed9d5bb90256ed7ddb
Dariusz Górak <darekg11@tlen.pl>
2020-05-03 16:07:38 +0200

    Add JR C n Code support

---
+++ core/src/components/cpu/opcodes/JMP/JMP_RELATIVE_IF_CARRY_FLAG_IS_SET/index.ts
@@ -0,0 +1,22 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0x38
+  Memonic: JR C, n
+  Description: Jump to relative address (PC + n) if Carry Flag is set.
+  Size: 2 Byte - increments PC by 2 if jump is not made
+  Cycles: 12 if jump is taken, 8 if it is not
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const isCarryFlagSet = payload.CPU.isCarryFlagSet();
+    if (isCarryFlagSet) {
+        const currentProgramCounter = payload.CPU.getProgramCounter();
+        const memoryValue = payload.Memory.read8BitsValue(currentProgramCounter + 1);
+        payload.CPU.jump(currentProgramCounter + memoryValue);
+    } else {
+        payload.CPU.increaseProgramCounter(2);
+    }
+}
+
+export default handle;
