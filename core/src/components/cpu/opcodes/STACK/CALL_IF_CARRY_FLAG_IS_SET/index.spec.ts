ed49831d4c53dde4064e3b2a4843962e8c7d6baa
Dariusz Górak <darekg11@tlen.pl>
2020-05-05 18:37:07 +0200

    Add CALL C, nn Code support

---
+++ core/src/components/cpu/opcodes/STACK/CALL_IF_CARRY_FLAG_IS_SET/index.spec.ts
@@ -0,0 +1,109 @@
+import handle from "./index";
+import CPU from "../../../cpu";
+import Memory from "../../../../memory/memory";
+
+describe("CALL_IF_CARRY_FLAG_IS_SET", () => {
+    test("Should CALL if carry flag is set", () => {
+        const cpu = new CPU();
+        const memory = new Memory();
+        const PC = 56789;
+        const SP = 100;
+        const CALL_VALUE = 120;
+        const EXPECTED_PC = CALL_VALUE;
+        const EXPECTED_SP = SP - 2;
+        const EXPECTED_F_REG_VALUE = 0b00010000;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setCarryFlag();
+        cpu.jump(PC);
+        cpu.setRegisterSPValue(SP);
+        memory.write16BitsValue(PC + 1, CALL_VALUE);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getProgramCounter()).toBe(EXPECTED_PC);
+        expect(cpu.getRegisterSPValue()).toBe(EXPECTED_SP);
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(true);
+        expect(cpu.isHalfCarryFlagSet()).toBe(false);
+        expect(memory.read8BitsValue(cpu.getRegisterSPValue())).toBe(PC >> 8);
+        expect(memory.read8BitsValue(cpu.getRegisterSPValue() + 1)).toBe(PC & 0xFF);
+        expect(memory.read16BitsValue(cpu.getRegisterSPValue())).toBe(PC);
+    });
+
+    test("Should NOT CALL if carry flag is not set", () => {
+        const cpu = new CPU();
+        const memory = new Memory();
+        const PC = 56789;
+        const SP = 100;
+        const CALL_VALUE = 120;
+        const EXPECTED_PC = PC + 3;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.unsetCarryFlag();
+        cpu.jump(PC);
+        cpu.setRegisterSPValue(SP);
+        memory.write16BitsValue(PC + 1, CALL_VALUE);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(EXPECTED_PC);
+        expect(cpu.getRegisterSPValue()).toBe(SP);
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(false);
+        expect(cpu.isHalfCarryFlagSet()).toBe(false);
+        expect(memory.read8BitsValue(cpu.getRegisterSPValue())).toBe(0);
+        expect(memory.read8BitsValue(cpu.getRegisterSPValue() + 1)).toBe(0);
+        expect(memory.read16BitsValue(cpu.getRegisterSPValue())).toBe(0);
+    });
+})
---
+++ core/src/components/cpu/opcodes/STACK/CALL_IF_CARRY_FLAG_IS_SET/index.ts
@@ -0,0 +1,24 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0xDC
+  Memonic: CALL C, nn
+  Description: Call relative address (nn) if Carry Flag is set.
+  Size: 3 Byte - increments PC by 3 if jump is not made
+  Cycles: 24 if jump is taken, 12 if it is not
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const isCarryFlagSet = payload.CPU.isCarryFlagSet();
+    const currentProgramCounter = payload.CPU.getProgramCounter();
+    const address = payload.Memory.read16BitsValue(currentProgramCounter + 1);
+    if (isCarryFlagSet) {
+        payload.CPU.decreaseStackPointer(2);
+        payload.Memory.write16BitsValue(payload.CPU.getRegisterSPValue(), currentProgramCounter);
+        payload.CPU.jump(address);
+    } else {
+        payload.CPU.increaseProgramCounter(3);
+    }
+}
+
+export default handle;
