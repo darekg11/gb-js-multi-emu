fdfd9a103c90c84ed3354596831dbe9ed50effc4
Dariusz Górak <darekg11@tlen.pl>
2020-05-06 19:06:09 +0200

    Add RET Z Code support

---
+++ core/src/components/cpu/opcodes/STACK/RETURN_IF_ZERO_FLAG_IS_SET/index.spec.ts
@@ -0,0 +1,105 @@
+import handle from "./index";
+import CPU from "../../../cpu";
+import Memory from "../../../../memory/memory";
+
+describe("RETURN_IF_ZERO_FLAG_IS_SET", () => {
+    test("Should RETURN if zero flag is set", () => {
+        const cpu = new CPU();
+        const memory = new Memory();
+        const PC = 56789;
+        const SP = 100;
+        const RETURN_VALUE = 56789;
+        const EXPECTED_SP = SP + 2;
+        const EXPECTED_F_REG_VALUE = 0b10000000;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setZeroFlag();
+        cpu.jump(PC);
+        cpu.setRegisterSPValue(SP);
+        memory.write16BitsValue(SP, RETURN_VALUE);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getProgramCounter()).toBe(RETURN_VALUE);
+        expect(cpu.getRegisterSPValue()).toBe(EXPECTED_SP);
+        expect(cpu.isZeroFlagSet()).toBe(true);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(false);
+        expect(cpu.isHalfCarryFlagSet()).toBe(false);
+        expect(memory.read8BitsValue(SP)).toBe(RETURN_VALUE >> 8);
+        expect(memory.read8BitsValue(SP + 1)).toBe(RETURN_VALUE & 0xFF);
+        expect(memory.read16BitsValue(SP)).toBe(RETURN_VALUE);
+    });
+
+    test("Should NOT RETURN if zero flag is NOT set", () => {
+        const cpu = new CPU();
+        const memory = new Memory();
+        const PC = 56789;
+        const SP = 100;
+        const RETURN_VALUE = 56789;
+        const EXPECTED_PC = PC + 1;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.unsetZeroFlag();
+        cpu.jump(PC);
+        cpu.setRegisterSPValue(SP);
+        memory.write16BitsValue(SP, RETURN_VALUE);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(EXPECTED_PC);
+        expect(cpu.getRegisterSPValue()).toBe(SP);
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(false);
+        expect(cpu.isHalfCarryFlagSet()).toBe(false);
+    });
+})
---
+++ core/src/components/cpu/opcodes/STACK/RETURN_IF_ZERO_FLAG_IS_SET/index.ts
@@ -0,0 +1,22 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0xC8
+  Memonic: RET Z
+  Description: Returns from function if Zero Flag is set.
+  Size: 1 Byte - increments PC by 1 if return is not executed
+  Cycles: 20 if return is made, 8 if it is not
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const isZeroFlagSet = payload.CPU.isZeroFlagSet();
+    if (isZeroFlagSet) {
+        const address = payload.Memory.read16BitsValue(payload.CPU.getRegisterSPValue());
+        payload.CPU.increaseStackPointer(2);
+        payload.CPU.jump(address);
+    } else {
+        payload.CPU.increaseProgramCounter(1);
+    }
+}
+
+export default handle;
