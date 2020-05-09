1ceb10e6b3b2bc2adccea83c3e5ffc216a4ce39c
Dariusz Górak <darekg11@tlen.pl>
2020-05-09 15:38:11 +0200

    Add RST_30 Code support

--- core/src/components/cpu/opcodes/OP_CODES_ENUM.ts
+++ core/src/components/cpu/opcodes/OP_CODES_ENUM.ts
@@ -249,6 +249,7 @@ export enum NON_PREFIX_OP_CODES {
     RST_18 = 0xDF,
     RST_20 = 0xE7,
     RST_28 = 0xEF,
+    RST_30 = 0xF7,
 
     CALL_IF_ZERO_FLAG_IS_NOT_SET = 0xC4,
     CALL_IF_ZERO_FLAG_IS_SET = 0xCC,
---
+++ core/src/components/cpu/opcodes/STACK/RST_30/index.ts
@@ -0,0 +1,18 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0xF7
+  Memonic: RST 30
+  Description: Call 0x30 address.
+  Size: 1 Byte - increments PC by 1 if jump is not made
+  Cycles: 16
+  Flags affected: None
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const currentProgramCounter = payload.CPU.getProgramCounter();
+    payload.CPU.decreaseStackPointer(2);
+    payload.Memory.write16BitsValue(payload.CPU.getRegisterSPValue(), currentProgramCounter);
+    payload.CPU.jump(0x30);
+}
+
+export default handle;
