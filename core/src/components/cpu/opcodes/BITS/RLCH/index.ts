627c43c48b90794aeac198ca31d16f233f4a8981
Dariusz Górak <darekg11@tlen.pl>
2020-05-16 21:14:55 +0200

    Add RLCH OP Code Support

--- core/src/components/cpu/opcodes/BITS/RLCH/index.ts
+++ core/src/components/cpu/opcodes/BITS/RLCH/index.ts
@@ -1,28 +1,28 @@
 import { IOpCodeHanlePayload } from "../../types";
 
 /*
-  OP Code: 0x05
-  Memonic: RLCL
-  Description: Shifts register L by 1 bit to the left.
+  OP Code: 0x04
+  Memonic: RLCH
+  Description: Shifts register H by 1 bit to the left.
   Carry flag is set to value of bit 7 of register L.
   Zero flag is set if result is 0
   Size: 2 Byte - increments PC by 2
   Cycles: 4
   Flags affected:
     Sets SUBTRACTION flag to 0
-    Sets CARRY flag is set to value of bit 7 of register L.
+    Sets CARRY flag is set to value of bit 7 of register H.
     Sets ZERO flag if result is 0
     Sets HALF_CARRY flag to 0
 
 */
 const handle = (payload: IOpCodeHanlePayload) => {
-    if (payload.CPU.getRegisterLValue() > 0x7F) {
+    if (payload.CPU.getRegisterHValue() > 0x7F) {
         payload.CPU.setCarryFlag();
     } else {
         payload.CPU.unsetCarryFlag();
     }
-    payload.CPU.setRegisterLValue((payload.CPU.getRegisterLValue() << 1 & 0xFF) | (payload.CPU.isCarryFlagSet() ? 1 : 0));
-    if (payload.CPU.getRegisterLValue() === 0) {
+    payload.CPU.setRegisterHValue((payload.CPU.getRegisterHValue() << 1 & 0xFF) | (payload.CPU.isCarryFlagSet() ? 1 : 0));
+    if (payload.CPU.getRegisterHValue() === 0) {
         payload.CPU.setZeroFlag();
     } else {
         payload.CPU.unsetZeroFlag();
