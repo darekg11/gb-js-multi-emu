768843a5b9a697601c08a68d4f8bb8b26528f92f
Dariusz Górak <darekg11@tlen.pl>
2020-05-17 18:24:18 +0200

    Add RRCD OP Code Support

---
+++ core/src/components/cpu/opcodes/BITS/RRCD/index.spec.ts
@@ -0,0 +1,98 @@
+import handle from "./index";
+import CPU from "../../../cpu";
+import Memory from "../../../../memory/memory";
+
+describe("RRCC", () => {
+    test("Should rotate register D value to the right by 1 bit. Should set carry flag if bit 0 of register D is set.", () => {
+        const REG_D_VALUE = 33;
+        // 33 >> 1 gives 16
+        // 16 & 255 === 16
+        // Moving bit 0 to bit 7 by doing << 7 bumps this value by 128
+        // 16 + 128 == 144
+        const EXPETCTED_REG_D_VALUE = 144;
+        const cpu = new CPU();
+        const memory = new Memory();
+        const EXPECTED_F_REG_VALUE = 0b00010000;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setRegisterDValue(REG_D_VALUE);
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(EXPETCTED_REG_D_VALUE);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(EXPETCTED_REG_D_VALUE << 8);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(EXPECTED_F_REG_VALUE );
+        expect(cpu.getProgramCounter()).toBe(2);
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isHalfCarryFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(true);
+    });
+
+    test("Should rotate register D value to the right by 1 bit. Should unset carry flag if bit 0 of register D is not set.", () => {
+        const REG_D_VALUE = 32;
+        const EXPETCTED_REG_D_VALUE = 16;
+        const cpu = new CPU();
+        const memory = new Memory();
+        const EXPECTED_F_REG_VALUE = 0b00000000;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setZeroFlag();
+        cpu.setRegisterDValue(REG_D_VALUE);
+        cpu.setCarryFlag();
+
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(EXPETCTED_REG_D_VALUE);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(EXPETCTED_REG_D_VALUE << 8);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getProgramCounter()).toBe(2);
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.isSubtractionFlagSet()).toBe(false);
+        expect(cpu.isHalfCarryFlagSet()).toBe(false);
+        expect(cpu.isCarryFlagSet()).toBe(false);
+    });
+
+})
