3d288bd8e87396bc64558e76d6798dceb5a3683f
Dariusz Górak <darekg11@tlen.pl>
2020-05-18 20:41:08 +0200

    Add RLD OP Code Support

---
+++ core/src/components/cpu/opcodes/BITS/RLD/index.ts
@@ -0,0 +1,36 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0x12
+  Memonic: RLD
+  Description: Shifts register D by 1 bit to the left.
+  Carry flag is set to value of bit 7 of register D.
+  Zero flag is set if result is 0
+  Size: 2 Byte - increments PC by 2
+  Cycles: 4
+  Flags affected:
+    Sets SUBTRACTION flag to 0
+    Sets CARRY flag is set to value of bit 7 of register D.
+    Sets ZERO flag if result is 0
+    Sets HALF_CARRY flag to 0
+
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const newCarryFlag = payload.CPU.getRegisterDValue() > 0x7F;
+    payload.CPU.setRegisterDValue((payload.CPU.getRegisterDValue() << 1 & 0xFF) | (payload.CPU.isCarryFlagSet() ? 1 : 0));
+    if (newCarryFlag) {
+        payload.CPU.setCarryFlag();
+    } else {
+        payload.CPU.unsetCarryFlag();
+    }
+    if (payload.CPU.getRegisterDValue() === 0) {
+        payload.CPU.setZeroFlag();
+    } else {
+        payload.CPU.unsetZeroFlag();
+    }
+    payload.CPU.unsetSubtractionFlag();
+    payload.CPU.unsetHalfCarryFlag();
+    payload.CPU.increaseProgramCounter(2);
+}
+
+export default handle;
--- core/src/components/cpu/opcodes/OP_CODES_ENUM.ts
+++ core/src/components/cpu/opcodes/OP_CODES_ENUM.ts
@@ -294,4 +294,5 @@ export enum CB_OP_CODES {
 
     RLB = 0x10,
     RLC = 0x11,
+    RLD = 0x12
 }
