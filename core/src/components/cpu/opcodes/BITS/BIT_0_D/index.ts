06ad09ac4111004a59a4e699ef5e779932505df9
Dariusz Górak <darekg11@tlen.pl>
2020-05-31 11:17:07 +0200

    Add BIT 0 D OP  Code Support

---
+++ core/src/components/cpu/opcodes/BITS/BIT_0_D/index.ts
@@ -0,0 +1,28 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0x42
+  Memonic: BIT 0 D
+  Description: Sets zero flag if bit 0 (top right) of register D is set to 0.
+  Size: 2 Byte - increments PC by 2
+  Cycles: 8
+  Flags affected:
+    Sets SUBTRACTION flag to 0.
+    Carry flag is unchanged.
+    Sets ZERO flag to 1 if bit 0 of register D is 0.
+    Sets HALF_CARRY flag to 1.
+
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerDValue = payload.CPU.getRegisterDValue();
+    if ((registerDValue & 0x01) === 0) {
+        payload.CPU.setZeroFlag();
+    } else {
+        payload.CPU.unsetZeroFlag();
+    }
+    payload.CPU.unsetSubtractionFlag();
+    payload.CPU.setHalfCarryFlag();
+    payload.CPU.increaseProgramCounter(2);
+}
+
+export default handle;
