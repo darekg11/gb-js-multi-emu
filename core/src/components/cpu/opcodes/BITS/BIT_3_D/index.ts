f107a3c5bd16b89dd5255da312bfa9071a1227b6
Dariusz Górak <darekg11@tlen.pl>
2020-06-04 18:14:29 +0200

    Add BIT 3 D OP Code Support

---
+++ core/src/components/cpu/opcodes/BITS/BIT_3_D/index.ts
@@ -0,0 +1,28 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0x5A
+  Memonic: BIT 3 D
+  Description: Sets zero flag if bit 3 (fourth to the top right) of register D is set to 0.
+  Size: 2 Byte - increments PC by 2
+  Cycles: 8
+  Flags affected:
+    Sets SUBTRACTION flag to 0.
+    Carry flag is unchanged.
+    Sets ZERO flag to 1 if bit 3 of register D is 0.
+    Sets HALF_CARRY flag to 1.
+
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerDValue = payload.CPU.getRegisterDValue();
+    if ((registerDValue & 0x08) === 0) {
+        payload.CPU.setZeroFlag();
+    } else {
+        payload.CPU.unsetZeroFlag();
+    }
+    payload.CPU.unsetSubtractionFlag();
+    payload.CPU.setHalfCarryFlag();
+    payload.CPU.increaseProgramCounter(2);
+}
+
+export default handle;
