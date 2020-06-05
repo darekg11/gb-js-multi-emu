513a94a37b93acbb25da593dc9447d4e6e98638b
Dariusz Górak <darekg11@tlen.pl>
2020-06-05 22:07:28 +0200

    Add BIT 4 L OP Code Support

---
+++ core/src/components/cpu/opcodes/BITS/BIT_4_L/index.ts
@@ -0,0 +1,28 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0x65
+  Memonic: BIT 4 L
+  Description: Sets zero flag if bit 4 (fifth to the right) of register L is set to 0.
+  Size: 2 Byte - increments PC by 2
+  Cycles: 8
+  Flags affected:
+    Sets SUBTRACTION flag to 0.
+    Carry flag is unchanged.
+    Sets ZERO flag to 1 if bit 4 of register L is 0.
+    Sets HALF_CARRY flag to 1.
+
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerLValue = payload.CPU.getRegisterLValue();
+    if ((registerLValue & 0x10) === 0) {
+        payload.CPU.setZeroFlag();
+    } else {
+        payload.CPU.unsetZeroFlag();
+    }
+    payload.CPU.unsetSubtractionFlag();
+    payload.CPU.setHalfCarryFlag();
+    payload.CPU.increaseProgramCounter(2);
+}
+
+export default handle;
