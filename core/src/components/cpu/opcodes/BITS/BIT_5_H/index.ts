6564beb2be8ba97926a70ea5d443657766ee76b5
Dariusz Górak <darekg11@tlen.pl>
2020-06-06 12:27:03 +0200

    Add BIT 5 H OP Code Support

---
+++ core/src/components/cpu/opcodes/BITS/BIT_5_H/index.ts
@@ -0,0 +1,28 @@
+import { IOpCodeHanlePayload } from "../../types";
+
+/*
+  OP Code: 0x6C
+  Memonic: BIT 5 H
+  Description: Sets zero flag if bit 5 (sixth most to the right) of register H is set to 0.
+  Size: 2 Byte - increments PC by 2
+  Cycles: 8
+  Flags affected:
+    Sets SUBTRACTION flag to 0.
+    Carry flag is unchanged.
+    Sets ZERO flag to 1 if bit 5 of register H is 0.
+    Sets HALF_CARRY flag to 1.
+
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerHValue = payload.CPU.getRegisterHValue();
+    if ((registerHValue & 0x20) === 0) {
+        payload.CPU.setZeroFlag();
+    } else {
+        payload.CPU.unsetZeroFlag();
+    }
+    payload.CPU.unsetSubtractionFlag();
+    payload.CPU.setHalfCarryFlag();
+    payload.CPU.increaseProgramCounter(2);
+}
+
+export default handle;
