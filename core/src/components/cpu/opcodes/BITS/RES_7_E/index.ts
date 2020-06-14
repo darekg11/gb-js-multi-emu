3bc34e86f3a0808b02ec7965f682397ba328265d
Dariusz Górak <darekg11@tlen.pl>
2020-06-14 15:16:11 +0200

    Add RES 7 E OP Code Support

---
+++ core/src/components/cpu/opcodes/BITS/RES_7_E/index.ts
@@ -0,0 +1,21 @@
+import { IOpCodeHanlePayload } from "../../types";
+import { numberUtils } from "../../../../../utils";
+
+/*
+  OP Code: 0xBB
+  Memonic: RES 7 E
+  Description: Sets bit 7 (first to the left) of register E to 0.
+  Size: 2 Byte - increments PC by 2
+  Cycles: 8
+  Flags affected:
+    None
+
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerEValue = payload.CPU.getRegisterEValue();
+    const newValue = numberUtils.unsetBit(registerEValue, 7);
+    payload.CPU.setRegisterEValue(newValue);
+    payload.CPU.increaseProgramCounter(2);
+}
+
+export default handle;
