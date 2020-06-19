2d0b59eaa51fe196f7498bacdd4c0feffc1524e1
Dariusz Górak <darekg11@tlen.pl>
2020-06-19 14:51:58 +0200

    Add SET 4 L OP Code Support

---
+++ core/src/components/cpu/opcodes/BITS/SET_4_L/index.ts
@@ -0,0 +1,21 @@
+import { IOpCodeHanlePayload } from "../../types";
+import { numberUtils } from "../../../../../utils";
+
+/*
+  OP Code: 0xE5
+  Memonic: SET 4 L
+  Description: Sets bit 4 (fifth to the right) of register L to 1.
+  Size: 2 Byte - increments PC by 2
+  Cycles: 8
+  Flags affected:
+    None
+
+*/
+const handle = (payload: IOpCodeHanlePayload) => {
+    const registerLValue = payload.CPU.getRegisterLValue();
+    const newValue = numberUtils.setBit(registerLValue, 4);
+    payload.CPU.setRegisterLValue(newValue);
+    payload.CPU.increaseProgramCounter(2);
+}
+
+export default handle;
