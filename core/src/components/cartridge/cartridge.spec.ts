533b15300398d6a137e7dfb05a58d2f6ff16dee8
Dariusz Górak <darekg11@tlen.pl>
2020-08-21 21:20:02 +0200

    Add unit tests for initializeCartrideType method

--- core/src/components/cartridge/cartridge.spec.ts
+++ core/src/components/cartridge/cartridge.spec.ts
@@ -1,5 +1,7 @@
 import _ from "lodash";
 import Cartridge from "./cartridge";
+import { CARTRIGDE_TYPES } from "./types";
+import UnsupportedCartridgeTypeError from "../../errors/UnsupportedCartridgeTypeError";
 
 describe("initializeProgramName", () => {
     test("Should read program name and store it in class variable", () => {
@@ -119,3 +121,265 @@ describe("initializeLicenseCode", () => {
         expect(program.getLicenseCode()).toBe("01");
     });
 });
+
+describe("initializeCartrideType", () => {
+    test("Value of 0x00 should set ROM_ONLY type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x00);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.ROM_ONLY);
+    });
+
+    test("Value of 0x01 should set MBC_1 type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x01);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_1);
+    });
+
+    test("Value of 0x02 should set MBC_1_RAM type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x02);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_1_RAM);
+    });
+
+    test("Value of 0x03 should set MBC_1_RAM_BATTERY type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x03);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_1_RAM_BATTERY);
+    });
+
+    test("Value of 0x05 should set MBC_2 type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x05);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_2);
+    });
+
+    test("Value of 0x06 should set MBC_2_BATTERY type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x06);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_2_BATTERY);
+    });
+
+    test("Value of 0x08 should set ROM_RAM type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x08);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.ROM_RAM);
+    });
+
+    test("Value of 0x09 should set ROM_RAM_BATTERY type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x09);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.ROM_RAM_BATTERY);
+    });
+
+    test("Value of 0x0B should set MMM01 type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x0B);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MMM_01);
+    });
+
+    test("Value of 0x0C should set MMM01_RAM type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x0C);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MMM_01_RAM);
+    });
+
+    test("Value of 0x0D should set MMM01_RAM_BATTERY type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x0D);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MMM_01_RAM_BATTERY);
+    });
+
+    test("Value of 0x0F should set MBC3_TIMER_BATTERY type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x0F);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_3_TIMER_BATTERY);
+    });
+
+    test("Value of 0x10 should set MBC3_TIMER_RAM_BATTERY type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x10);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_3_TIMER_RAM_BATTERY);
+    });
+
+    test("Value of 0x11 should set MBC3 type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x11);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_3);
+    });
+
+    test("Value of 0x12 should set MBC3_RAM type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x12);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_3_RAM);
+    });
+
+    test("Value of 0x13 should set MBC3_RAM_BATTERY type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x13);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_3_RAM_BATTERY);
+    });
+
+    test("Value of 0x19 should set MBC5 type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x19);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_5);
+    });
+
+    test("Value of 0x1A should set MBC5_RAM type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x1A);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_5_RAM);
+    });
+
+    test("Value of 0x1B should set MBC5_RAM_BATTERY type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x1B);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_5_RAM_BATTERY);
+    });
+
+    test("Value of 0x1C should set MBC5_RUMBLE type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x1C);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_5_RUMBLE);
+    });
+
+    test("Value of 0x1D should set MBC5_RUMBLE_RAM type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x1D);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_5_RUMBLE_RAM);
+    });
+
+    test("Value of 0x1E should set MBC5_RUMBLE_RAM_BATTERY type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x1E);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_5_RUMBLE_RAM_BATTERY);
+    });
+
+    test("Value of 0x20 should set MBC6 type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x20);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_6);
+    });
+
+    test("Value of 0x22 should set MBC_7_SENSOR_RUMBLE_RAM_BATTERY type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0x22);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.MBC_7_SENSOR_RUMBLE_RAM_BATTERY);
+    });
+
+    test("Value of 0xFC should set POCKET_CAMERA type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0xFC);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.POCKET_CAMERA);
+    });
+
+    test("Value of 0xFD should set BANDAI_TAMA_5 type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0xFD);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.BANDAI_TAMA_5);
+    });
+
+    test("Value of 0xFE should set HUC_3 type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0xFE);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.HUC_3);
+    });
+
+    test("Value of 0xFF should set HUC_1_RAM_BATTERY type", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0xFF);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+        const program = new Cartridge(EXAMPLE_PROGRAM);
+
+        expect(program.getCartridgeType()).toBe(CARTRIGDE_TYPES.HUC_1_RAM_BATTERY);
+    });
+
+    test("Not suported value should throw error", () => {
+        const EXAMPLE_PROGRAM = _.range(0x147);
+        EXAMPLE_PROGRAM.push(0xDD);
+        EXAMPLE_PROGRAM.push(..._.range(0x400));
+
+        expect(() => new Cartridge(EXAMPLE_PROGRAM)).toThrowError(UnsupportedCartridgeTypeError);
+    });
+})
--- core/src/components/cartridge/cartridge.ts
+++ core/src/components/cartridge/cartridge.ts
@@ -275,7 +275,6 @@ class Cartridge {
                 break;
             }
         }
-
     }
 
     private initialize = () => {
