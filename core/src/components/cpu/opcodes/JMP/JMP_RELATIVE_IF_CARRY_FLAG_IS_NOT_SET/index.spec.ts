58778b95f69ab6c6d63b26238363c66f5980f893
Dariusz Górak <darekg11@tlen.pl>
2020-10-23 17:57:50 +0200

    Fix JR NC e OP Code. The next value from memory is supposed to be treated as signed byte, additionally jump is taking place from address of instruction that is suppose to be executed after e

--- core/src/components/cpu/opcodes/JMP/JMP_RELATIVE_IF_CARRY_FLAG_IS_NOT_SET/index.spec.ts
+++ core/src/components/cpu/opcodes/JMP/JMP_RELATIVE_IF_CARRY_FLAG_IS_NOT_SET/index.spec.ts
@@ -8,7 +8,7 @@ describe("JMP_RELATIVE_IF_CARRY_FLAG_IS_NOT_SET", () => {
         const memory = new Memory();
         const PC = 10;
         const RELATIVE_JMP_VALUE = 120;
-        const EXPECTED_PC = PC + RELATIVE_JMP_VALUE;
+        const EXPECTED_PC = PC + RELATIVE_JMP_VALUE + 2;
         expect(cpu.getRegisterAValue()).toBe(0);
         expect(cpu.getRegisterBValue()).toBe(0);
         expect(cpu.getRegisterCValue()).toBe(0);
