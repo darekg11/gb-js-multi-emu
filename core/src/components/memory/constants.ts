1efface9f3d87c905b7bff154c0f8a5ea06e7d6d
Dariusz Górak <darekg11@tlen.pl>
2020-11-11 19:00:45 +0100

    More BIOS description and registers constants

--- core/src/components/memory/constants.ts
+++ core/src/components/memory/constants.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+
 const BACKGROUND_PALLETE_REGISTER = 0xFF47; // READ / WRITE
 const OBJECT_PALLETE_ZERO_REGISTER = 0xFF48; // READ / WRITE
 const OBJECT_PALLETE_ONE_REGISTER = 0xFF49; // READ / WRITE
@@ -14,4 +16,29 @@ const WINDOW_X_REGISTER = 0xFF4B; // READ / WRITE
 // The window becomes visible (if enabled) when positions are set in range WX=0..166, WY=0..143. A postion of WX=7, WY=0 locates the window at upper left, it is then completly covering normal background.
 
 const VRAM_AREA_START_INDEX = 0x8000;
-const VRAM_AREA_END_INDEX = 0x9FFF; 
+const VRAM_AREA_END_INDEX = 0x9FFF;
+
+const UNMAP_ROM_REGISTER = 0xFF50; // Set to non-zero to disable boot ROM unmapping it and moving ROM to the begginig of memory -> done at the end of BIOS sequence.
+
+const REGISTERS_MAP = {
+    GPU: {
+        BACKGROUND_PALLETE_REGISTER,
+        OBJECT_PALLETE_ZERO_REGISTER,
+        OBJECT_PALLETE_ONE_REGISTER,
+        SCROLL_Y_REGISTER,
+        SCROLL_X_REGISTER,
+        LY_REGISTER,
+        LYC_REGISTER,
+        WINDOW_Y_REGISTER,
+        WINDOW_X_REGISTER
+    },
+    MEMORY: {
+        VRAM_AREA_START_INDEX,
+        VRAM_AREA_END_INDEX
+    },
+    MISC: {
+        UNMAP_ROM_REGISTER
+    }
+}
+
+export default _.cloneDeep(REGISTERS_MAP);
