2d368c0e30dcf32825516176ac5124bbc8dabe59
Dariusz Górak <darekg11@tlen.pl>
2021-01-01 15:15:29 +0100

    Fix ADC_TO_REG_A_VALUE_OF_REG_A OP Code

--- core/src/components/cpu/opcodes/MATH/ADC_TO_REG_A_VALUE_OF_REG_A/index.spec.ts
+++ core/src/components/cpu/opcodes/MATH/ADC_TO_REG_A_VALUE_OF_REG_A/index.spec.ts
@@ -4,11 +4,23 @@ import Memory from "../../../../memory/memory";
 import EventBus from "../../../../event-bus";
 
 describe("ADC_TO_REG_A_VALUE_OF_REG_A", () => {
-    test("Should add value of reg A to reg A, increase PC by 1 and Subtraction flag to 0", () => {
+    test("Result above 255 should set carry flag - carry flag NOT set before", () => {
+    });
+
+    test("Result above 255 should set carry flag - carry flag SET before", () => {
+    });
+
+    test("Result below 0 should set carry flag - carry flag NOT set before", () => {
+    });
+
+    test("Result below 0 should set carry flag - carry flag SET before", () => {
+    });
+
+    test("Result between 0 and 255 should not set carry flag - carry flag NOT set before", () => {
         const cpu = new CPU();
         const memory = new Memory(new EventBus);
-        const REG_A = 2;
-        const RESULT = REG_A + REG_A;
+        const REG_A = 10;
+        const RESULT = 20;
         expect(cpu.getRegisterAValue()).toBe(0);
         expect(cpu.getRegisterBValue()).toBe(0);
         expect(cpu.getRegisterCValue()).toBe(0);
@@ -22,79 +34,20 @@ describe("ADC_TO_REG_A_VALUE_OF_REG_A", () => {
         expect(cpu.getRegisterHLValue()).toBe(0);
         expect(cpu.getRegisterAFValue()).toBe(0);
         expect(cpu.getProgramCounter()).toBe(0);
-
+    
         cpu.setRegisterAValue(REG_A);
 
         handle({ CPU: cpu, Memory: memory });
-
-        expect(cpu.getRegisterAValue()).toBe(RESULT);
-        expect(cpu.getRegisterBValue()).toBe(0);
-        expect(cpu.getRegisterCValue()).toBe(0);
-        expect(cpu.getRegisterDValue()).toBe(0);
-        expect(cpu.getRegisterEValue()).toBe(0);
-        expect(cpu.getRegisterFValue()).toBe(0);
-        expect(cpu.getRegisterHValue()).toBe(0);
-        expect(cpu.getRegisterLValue()).toBe(0);
-        expect(cpu.getRegisterBCValue()).toBe(0);
-        expect(cpu.getRegisterDEValue()).toBe(0);
-        expect(cpu.getRegisterHLValue()).toBe(0);
-        expect(cpu.getRegisterAFValue()).toBe(RESULT << 8);
-        expect(cpu.getProgramCounter()).toBe(1);
-        expect(cpu.isZeroFlagSet()).toBe(false);
-        expect(cpu.isSubtractionFlagSet()).toBe(false);
+    
         expect(cpu.isCarryFlagSet()).toBe(false);
-        expect(cpu.isHalfCarryFlagSet()).toBe(false);
-    });
-
-    test("Result of overflowing 255 should set carry flag, increase PC by 1 and Subtraction flag to 0", () => {
-        const cpu = new CPU();
-        const memory = new Memory(new EventBus);
-        const REG_A = 255;
-        const RESULT = 254;
-        const EXPECTED_F_REG_VALUE = 0b00110000;
-        expect(cpu.getRegisterAValue()).toBe(0);
-        expect(cpu.getRegisterBValue()).toBe(0);
-        expect(cpu.getRegisterCValue()).toBe(0);
-        expect(cpu.getRegisterDValue()).toBe(0);
-        expect(cpu.getRegisterEValue()).toBe(0);
-        expect(cpu.getRegisterFValue()).toBe(0);
-        expect(cpu.getRegisterHValue()).toBe(0);
-        expect(cpu.getRegisterLValue()).toBe(0);
-        expect(cpu.getRegisterBCValue()).toBe(0);
-        expect(cpu.getRegisterDEValue()).toBe(0);
-        expect(cpu.getRegisterHLValue()).toBe(0);
-        expect(cpu.getRegisterAFValue()).toBe(0);
-        expect(cpu.getProgramCounter()).toBe(0);
-
-        cpu.setRegisterAValue(REG_A);
-
-        handle({ CPU: cpu, Memory: memory });
-
         expect(cpu.getRegisterAValue()).toBe(RESULT);
-        expect(cpu.getRegisterBValue()).toBe(0);
-        expect(cpu.getRegisterCValue()).toBe(0);
-        expect(cpu.getRegisterDValue()).toBe(0);
-        expect(cpu.getRegisterEValue()).toBe(0);
-        expect(cpu.getRegisterFValue()).toBe(EXPECTED_F_REG_VALUE);
-        expect(cpu.getRegisterHValue()).toBe(0);
-        expect(cpu.getRegisterLValue()).toBe(0);
-        expect(cpu.getRegisterBCValue()).toBe(0);
-        expect(cpu.getRegisterDEValue()).toBe(0);
-        expect(cpu.getRegisterHLValue()).toBe(0);
-        expect(cpu.getRegisterAFValue()).toBe(RESULT << 8 | EXPECTED_F_REG_VALUE);
-        expect(cpu.getProgramCounter()).toBe(1);
-        expect(cpu.isZeroFlagSet()).toBe(false);
-        expect(cpu.isSubtractionFlagSet()).toBe(false);
-        expect(cpu.isCarryFlagSet()).toBe(true);
-        expect(cpu.isHalfCarryFlagSet()).toBe(true);
     });
 
-    test("Should add value of reg B to reg A, increase PC by 1 and Subtraction flag to 0. Should respect Carry Flag", () => {
+    test("Result between 0 and 255 should not set carry flag - carry flag SET before", () => {
         const cpu = new CPU();
         const memory = new Memory(new EventBus);
-        const REG_A = 2;
-        const RESULT = REG_A + REG_A + 1;
-        const EXPECTED_F_REG_VALUE = 0b00010000;
+        const REG_A = 10;
+        const RESULT = 20 + 1; // +1 from carry
         expect(cpu.getRegisterAValue()).toBe(0);
         expect(cpu.getRegisterBValue()).toBe(0);
         expect(cpu.getRegisterCValue()).toBe(0);
@@ -108,28 +61,200 @@ describe("ADC_TO_REG_A_VALUE_OF_REG_A", () => {
         expect(cpu.getRegisterHLValue()).toBe(0);
         expect(cpu.getRegisterAFValue()).toBe(0);
         expect(cpu.getProgramCounter()).toBe(0);
-
-        cpu.setRegisterAValue(REG_A);
+    
         cpu.setCarryFlag();
-
+        cpu.setRegisterAValue(REG_A);
+    
         handle({ CPU: cpu, Memory: memory });
-
+    
+        expect(cpu.isCarryFlagSet()).toBe(false);
         expect(cpu.getRegisterAValue()).toBe(RESULT);
+    });
+
+    test("Result 0 should set zero flag - carry flag NOT set before", () => {
+        const cpu = new CPU();
+        const memory = new Memory(new EventBus);
+        const REG_A = 0;
+        const RESULT = 0;
+        expect(cpu.getRegisterAValue()).toBe(0);
         expect(cpu.getRegisterBValue()).toBe(0);
         expect(cpu.getRegisterCValue()).toBe(0);
         expect(cpu.getRegisterDValue()).toBe(0);
         expect(cpu.getRegisterEValue()).toBe(0);
-        expect(cpu.getRegisterFValue()).toBe(EXPECTED_F_REG_VALUE);
+        expect(cpu.getRegisterFValue()).toBe(0);
         expect(cpu.getRegisterHValue()).toBe(0);
         expect(cpu.getRegisterLValue()).toBe(0);
         expect(cpu.getRegisterBCValue()).toBe(0);
         expect(cpu.getRegisterDEValue()).toBe(0);
         expect(cpu.getRegisterHLValue()).toBe(0);
-        expect(cpu.getRegisterAFValue()).toBe(RESULT << 8 | EXPECTED_F_REG_VALUE);
-        expect(cpu.getProgramCounter()).toBe(1);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+    
+        cpu.setRegisterAValue(REG_A);
+    
+        handle({ CPU: cpu, Memory: memory });
+    
+        expect(cpu.isZeroFlagSet()).toBe(true);
+        expect(cpu.getRegisterAValue()).toBe(RESULT);
+    });
+
+    test("Result above 0 should not set zero flag - carry flag NOT set before", () => {
+        const cpu = new CPU();
+        const memory = new Memory(new EventBus);
+        const REG_A = 10;
+        const RESULT = 20;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+    
+        cpu.setRegisterAValue(REG_A);
+    
+        handle({ CPU: cpu, Memory: memory });
+    
         expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.getRegisterAValue()).toBe(RESULT);
+    });
+
+    test("Result above 0 should not set zero flag - carry flag SET before", () => {
+        const cpu = new CPU();
+        const memory = new Memory(new EventBus);
+        const REG_A = 20;
+        const RESULT = 40 + 1; // +1 comes from carry
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+    
+        cpu.setCarryFlag();
+        cpu.setRegisterAValue(REG_A);
+    
+        handle({ CPU: cpu, Memory: memory });
+    
+        expect(cpu.isZeroFlagSet()).toBe(false);
+        expect(cpu.getRegisterAValue()).toBe(RESULT);
+    });
+
+    test("Result of 17 should set half carry flag - carry flag NOT set before", () => {
+        const cpu = new CPU();
+        const memory = new Memory(new EventBus);
+        const REG_A = -17;
+        const RESULT = 222;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+    
+        cpu.setRegisterAValue(REG_A);
+    
+        handle({ CPU: cpu, Memory: memory });
+    
+        expect(cpu.isHalfCarryFlagSet()).toBe(true);
+        expect(cpu.getRegisterAValue()).toBe(RESULT);
+    });
+
+    test("Result of 17 should set half carry flag - carry flag SET before", () => {
+        const cpu = new CPU();
+        const memory = new Memory(new EventBus);
+        const REG_A = -17;
+        const RESULT = 222 + 1; // +1 comes from carry
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+    
+        cpu.setCarryFlag();
+        cpu.setRegisterAValue(REG_A);
+    
+        handle({ CPU: cpu, Memory: memory });
+    
+        expect(cpu.isHalfCarryFlagSet()).toBe(true);
+        expect(cpu.getRegisterAValue()).toBe(RESULT);
+    });
+
+    test("PC should be increased by 1", () => {
+        const cpu = new CPU();
+        const memory = new Memory(new EventBus);
+        const REG_A = -17;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setRegisterAValue(REG_A);
+    
+        handle({ CPU: cpu, Memory: memory });
+
+        expect(cpu.getProgramCounter()).toBe(1);
+    });
+
+    test("Subtraction flag should NOT be set", () => {
+        const cpu = new CPU();
+        const memory = new Memory(new EventBus);
+        const REG_A = -17;
+        expect(cpu.getRegisterAValue()).toBe(0);
+        expect(cpu.getRegisterBValue()).toBe(0);
+        expect(cpu.getRegisterCValue()).toBe(0);
+        expect(cpu.getRegisterDValue()).toBe(0);
+        expect(cpu.getRegisterEValue()).toBe(0);
+        expect(cpu.getRegisterFValue()).toBe(0);
+        expect(cpu.getRegisterHValue()).toBe(0);
+        expect(cpu.getRegisterLValue()).toBe(0);
+        expect(cpu.getRegisterBCValue()).toBe(0);
+        expect(cpu.getRegisterDEValue()).toBe(0);
+        expect(cpu.getRegisterHLValue()).toBe(0);
+        expect(cpu.getRegisterAFValue()).toBe(0);
+        expect(cpu.getProgramCounter()).toBe(0);
+
+        cpu.setRegisterAValue(REG_A);
+    
+        handle({ CPU: cpu, Memory: memory });
+
         expect(cpu.isSubtractionFlagSet()).toBe(false);
-        expect(cpu.isCarryFlagSet()).toBe(true);
-        expect(cpu.isHalfCarryFlagSet()).toBe(false);
     });
 })
