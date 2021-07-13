cb7219f03bcafc0e7a9fe30e50d61de94d6e4a3e
Dariusz Górak <darekg11@tlen.pl>
2021-07-13 23:16:50 +0200

    Load ROM files, add input handling to main.html. Launch Tetris

--- desktop/src/main.ts
+++ desktop/src/main.ts
@@ -1,46 +1,71 @@
-import { app, BrowserWindow, Menu, MenuItemConstructorOptions } from "electron";
-import { LCD_WIDTH, LCD_HEIGHT } from "gb-js-multi-emu-core/dist/components/gpu/constants";
+import { app, BrowserWindow, dialog, Menu, MenuItemConstructorOptions } from "electron";
+import { LCD_HEIGHT } from "gb-js-multi-emu-core/dist/components/gpu/constants";
+import { LOAD_ROM_IPC_EVENT } from "./scripts/types";
+import fs from "fs";
 
-let mainWindowInstance = null;
 const isMac = process.platform === 'darwin';
 const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
-const mainWindowMenuTemplate: MenuItemConstructorOptions[] = [
-    {
-        label: "Emulator",
-        submenu: [{
-            label: "Load ROM"
+const buildMenu = (windowInstance: BrowserWindow) : MenuItemConstructorOptions[] => {
+    return [
+        {
+            label: "Emulator",
+            submenu: [{
+                label: "Load ROM",
+                click: async () => {
+                    const selectedFiles = dialog.showOpenDialogSync(windowInstance, {
+                        title: "Please select your ROM file",
+                        filters: [{
+                            name: "ROM", extensions: [ "bin", "gb" ]
+                        }],
+                        properties: [ "openFile" ]
+                    });
+                    if (selectedFiles && selectedFiles.length && selectedFiles[0]) {
+                        const romPath = selectedFiles[0];
+                        try {
+                            await fs.promises.access(romPath, fs.constants.F_OK | fs.constants.R_OK);
+                            const romBinaryData = await fs.promises.readFile(romPath, {
+                                encoding: null,
+                                flag: "r"
+                            });
+                            windowInstance.webContents.send(LOAD_ROM_IPC_EVENT, romBinaryData);
+                        } catch (error) {
+                            dialog.showErrorBox("Missing permissions!", "You don't have permissions required to open ROM file. Please check your ROM's permissions / groups.");
+                        }
+                    }
+                }
+            }, {
+                label: "Restart ROM"
+            }, {
+                type: "separator",
+            }, {
+                label: "Quit",
+                click: () => {
+                    app.quit();
+                }
+            }]
         }, {
-            label: "Restart ROM"
-        }, {
-            type: "separator",
-        }, {
-            label: "Quit",
-            click: () => {
-                app.quit();
-            }
-        }]
-    }, {
-        label: "Help",
-        submenu: [{
-            label: "Controls"
-        }, {
-            label: "Version Info"
-        }, {
-            label: "Check out on GitHub"
-        }]
-    }
-];
+            label: "Help",
+            submenu: [{
+                label: "Controls"
+            }, {
+                label: "Version Info"
+            }, {
+                label: "Check out on GitHub"
+            }]
+        }
+    ]
+}
 
-const mainWindowMenu = Menu.buildFromTemplate(mainWindowMenuTemplate);
+
 
 const createMainWindow = () => {
-    mainWindowInstance = new BrowserWindow({
+    const mainWindowInstance = new BrowserWindow({
         width: 400,
-        height: (LCD_HEIGHT * 2) + 20,
+        height: (LCD_HEIGHT * 2) + 50,
         title: "GB JS Multi Emu",
         maximizable: false,
-        maxHeight: (LCD_HEIGHT * 5) + 20,
+        resizable: false,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
@@ -48,6 +73,7 @@ const createMainWindow = () => {
         }
     });
     mainWindowInstance.loadURL(`file://${__dirname}/screens/main.html`);
+    const mainWindowMenu = Menu.buildFromTemplate(buildMenu(mainWindowInstance));
     Menu.setApplicationMenu(mainWindowMenu);
 }
 
