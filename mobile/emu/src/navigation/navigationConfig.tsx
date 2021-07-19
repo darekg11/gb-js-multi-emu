6c32a52b4828aa8bb60bfa9da62756974e2c73e4
Dariusz Górak <darekg11@tlen.pl>
2021-07-19 21:37:38 +0200

    Fix up styling in Drawer - now it looks slick

--- mobile/emu/src/navigation/navigationConfig.tsx
+++ mobile/emu/src/navigation/navigationConfig.tsx
@@ -30,7 +30,6 @@ const styles = StyleSheet.create({
   drawer: {
     borderRightWidth: StyleSheet.hairlineWidth,
     borderRightColor: stylesService.getDividerColor(),
-    width: '60%',
   },
 });
 
